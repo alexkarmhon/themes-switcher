1ec1b6c3378ac9e36fd460f8f24bc8e298d9101a
alexkarmhon <89stigmat89@gmail.com>
2024-01-14 20:07:37 +0200

    ./

--- src/js/switcher.js
+++ src/js/switcher.js
@@ -1,8 +1,27 @@
 const switcher = document.querySelector('#theme-switch-toggle');
-const menuLst = document.querySelector('#js-menu');
+const body = document.querySelector('body');
 
-const THEME = {
+const { LIGHT, DARK } = {
   LIGHT: 'light-theme',
   DARK: 'dark-theme'
-};
+}
 
+// Колір теми беремо з локалстора або за замовченням ЛАЙТ
+let themeColor = localStorage.getItem('theme-color') || LIGHT;
+body.classList.add(themeColor);
+
+// Якщо поточна тема ДАРК, то робимо switcher чекнутим
+switcher.checked = themeColor === DARK;
+
+function themeChanger() {
+  // Змінюємо поточну тему
+  body.classList.toggle(LIGHT);
+  body.classList.toggle(DARK);
+  // Зберігаємо змінене значення в локалстор
+  themeColor = themeColor === LIGHT ? DARK : LIGHT;
+  localStorage.setItem('theme-color', themeColor);
+  console.log("Theme is changes")
+}
+
+
+switcher.addEventListener('change', themeChanger);
