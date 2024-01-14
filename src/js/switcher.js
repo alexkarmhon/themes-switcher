154722aa4720731cb46a8b6d36c0aa63122041be
alexkarmhon <89stigmat89@gmail.com>
2024-01-14 19:09:50 +0200

    .

---
+++ src/js/switcher.js
@@ -0,0 +1,8 @@
+const switcher = document.querySelector('#theme-switch-toggle');
+const menuLst = document.querySelector('#js-menu');
+
+const THEME = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme'
+};
+
